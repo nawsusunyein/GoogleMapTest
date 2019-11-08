0db40cd2265d8a8eddefa8c2a63f85e85ab7f278
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-11-08 14:11:46 +0630

    add google map and set latitude and longitude

--- app/src/main/java/com/example/maptest/MapsActivity.java
+++ app/src/main/java/com/example/maptest/MapsActivity.java
@@ -40,8 +40,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(22, 96);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Myanmar"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 }
